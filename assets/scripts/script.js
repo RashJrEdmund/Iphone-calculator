ce4ccb24f11b40106c848ffdf43e01c469cdd37c
rash <orashusedmund@gmail.com>
2023-04-28 22:37:02 +0100

    done with the basic input inputs

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,27 +1,11 @@
 const numberButtons = document.querySelectorAll("[data-number");
-console.log(numberButtons);
-
 const operatorButtons = document.querySelectorAll("[data-operator]");
-const equalButtons = document.querySelector("[data-equals]");
 
+const equalsBtn = document.querySelector("[data-equals]");
 const backBtn = document.querySelector("[data-back]");
 const acButton = document.querySelector("[data-ac]");
 const showAns = document.querySelector("[data-show-ans]");
 const showWorking = document.querySelector("[data-show-working]");
-
-document.write("hello");
-
-numberButtons.forEach((button) => {
-  button.addEventListener("click", () => {
-    if (+showWorking.innerHTML === 0) {
-      showWorking.innerHTML = button.id;
-      return;
-    }
-
-    showWorking.append(button.id);
-    if (button.id === ".") button.disabled = true;
-  });
-});
 
 acButton.addEventListener("click", () => {
   showWorking.innerHTML = 0;
@@ -31,11 +15,43 @@ acButton.addEventListener("click", () => {
 backBtn.addEventListener("click", () => {
   let text = showWorking.innerHTML;
   text = text.slice(0, text.length - 1);
+
   if (!/\d/.test(+[...text].pop())) {
     document.getElementById(".").disabled = false;
     text = text.slice(0, text.length - 1);
   }
+  if (!text.includes(".")) document.getElementById(".").disabled = false;
 
   showWorking.innerHTML = text || 0;
-  // console.log(JSON.parse(showWorking.innerHTML), text);
+});
+
+numberButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    if (+showWorking.innerHTML === 0 && showWorking.innerHTML.length <= 1) {
+      console.log("inner check entered", showWorking.innerHTML.length);
+      if (button.id === ".") {
+        showWorking.innerHTML = "0" + button.id;
+        button.disabled = true;
+        return;
+      }
+
+      showWorking.innerHTML = button.id;
+      return;
+    }
+
+    showWorking.append(button.id);
+    if (button.id === ".") button.disabled = true;
+  });
+});
+
+operatorButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    if (+showWorking.innerHTML === 0) {
+      showWorking.innerHTML = button.id;
+      return;
+    }
+
+    showWorking.append(button.id);
+    if (button.id === ".") button.disabled = true;
+  });
 });
