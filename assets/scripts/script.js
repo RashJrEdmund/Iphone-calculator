0299bba2c536106e5bb322e13a1db601a06178f3
rash <orashusedmund@gmail.com>
2023-06-25 11:51:09 +0100

    done with basic functionality, debugging concatenation error

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,93 +1,101 @@
-import evaluate from "./service.js";
+import evaluate from './service.js'
 
-const numberButtons = document.querySelectorAll("[data-number");
-const operatorButtons = document.querySelectorAll("[data-operator]");
+const numberButtons = document.querySelectorAll('[data-number')
+const operatorButtons = document.querySelectorAll('[data-operator]')
 
-const equalsBtn = document.querySelector("[data-equals]");
-const backBtn = document.querySelector("[data-back]");
-const acButton = document.querySelector("[data-ac]");
-const showAns = document.querySelector("[data-show-ans]");
-const showWorking = document.querySelector("[data-show-working]");
+const equalsBtn = document.querySelector('[data-equals]')
+const backBtn = document.querySelector('[data-back]')
+const acButton = document.querySelector('[data-ac]')
+const showAns = document.querySelector('[data-show-ans]')
+const showWorking = document.querySelector('[data-show-working]')
 
 export const resHolder = {
   answer: null,
-  currOperator: "",
-};
+  firstValue: '',
+  secondValue: '',
+  currOperator: ''
+}
+
+// const operators = ['+', '-', '*', '÷', '%']
 
 const blinkShowWorking = () => {
-  showWorking.style.backgroundColor = "gold";
+  showWorking.style.backgroundColor = 'gold'
   setTimeout(() => {
-    showWorking.style.backgroundColor = "#000";
-  }, 50);
-};
+    showWorking.style.backgroundColor = '#000'
+  }, 50)
+}
 
-equalsBtn.addEventListener('click', blinkShowWorking)
+equalsBtn.addEventListener('click', () => {
+  resHolder.firstValue = resHolder.answer
+  resHolder.secondValue = ''
+  showAns.innerHTML = resHolder.answer
+  showWorking.innerHTML = resHolder.answer
+  blinkShowWorking()
+})
 
-acButton.addEventListener("click", () => {
-  showWorking.innerHTML = 0;
-  showAns.innerHTML = 0;
-  document.getElementById(".").disabled = false;
-  resHolder.answer = 0;
-  resHolder.currOperator = null;
-});
+acButton.addEventListener('click', () => {
+  showWorking.innerHTML = 0
+  showAns.innerHTML = 0
+  document.getElementById('.').disabled = false
+  resHolder.answer = 0
+  resHolder.currOperator = null
+})
 
-backBtn.addEventListener("click", () => {
-  let text = showWorking.innerHTML;
-  text = text.slice(0, -1);
+backBtn.addEventListener('click', () => {
+  const text = showWorking.innerHTML
 
-  if (!/\d/.test(+[...text].pop())) {
-    document.getElementById(".").disabled = false;
-    text = text.slice(0, text.length - 1);
-  }
+  if (isNaN(text.slice(-1))) document.getElementById('.').disabled = false
 
-  if (!text.includes(".")) document.getElementById(".").disabled = false;
+  if (!text.includes('.')) document.getElementById('.').disabled = false
 
-  showWorking.innerHTML = text || 0;
+  if (isNaN(text.slice(-2).slice(0, 1))) document.getElementById('.').disabled = true
+
+  showWorking.innerHTML = text.slice(0, -1) || 0
 
   if (resHolder.currOperator) {
-    const vals = showWorking.textContent.split(resHolder.currOperator);
-    evaluate(+vals[0], +vals[1] || 0, resHolder.currOperator);
+    const vals = showWorking.textContent.split(resHolder.currOperator)
+    evaluate(+vals[0], +vals[1] || 0, resHolder.currOperator)
   }
-});
+})
 
 numberButtons.forEach((button) => {
-  button.addEventListener("click", () => {
+  button.addEventListener('click', () => {
     if (+showWorking.innerHTML === 0 && showWorking.innerHTML.length <= 1) {
-      if (button.id === ".") {
-        showWorking.innerHTML = "0" + button.id;
-        button.disabled = true;
-        return;
+      if (button.id === '.') {
+        showWorking.innerHTML = '0' + button.id
+        button.disabled = true
+        return
       }
 
-      showWorking.innerHTML = +button.id;
-      return;
+      showWorking.innerHTML = +button.id
+      return
     }
 
-    showWorking.textContent += button.id;
-    if (button.id === ".") button.disabled = true;
+    showWorking.textContent += button.id
+    if (button.id === '.') button.disabled = true
 
     if (resHolder.currOperator) {
       console.log('operator found', resHolder.currOperator)
-      const vals = showWorking.textContent.split(resHolder.currOperator);
-    
-      evaluate(+vals[0], +vals[1] || 0, resHolder.currOperator);
+      const vals = showWorking.textContent.split(resHolder.currOperator)
+
+      evaluate(+vals[0], +vals[1] || 0, resHolder.currOperator)
     }
-  });
-});
+  })
+})
 
 operatorButtons.forEach((button) => {
-  button.addEventListener("click", () => {
-    if (showWorking.innerHTML.length <= 0) return;
+  button.addEventListener('click', () => {
+    if (showWorking.innerHTML.length <= 0) return
 
     if (resHolder.answer && resHolder.currOperator) {
-      showWorking.textContent = resHolder.answer;
+      showWorking.textContent = resHolder.answer
     }
 
-    showWorking.textContent += button.id;
+    showWorking.textContent += button.id
 
-    resHolder.currOperator = button.id;
+    resHolder.currOperator = button.id
 
-    blinkShowWorking();
-    document.getElementById(".").disabled = false;
-  });
-});
+    blinkShowWorking()
+    document.getElementById('.').disabled = false
+  })
+})
