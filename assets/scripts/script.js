5ce9ceac637796eb2d4988e77a7426a8ed2c0634
rash <orashusedmund@gmail.com>
2023-07-18 08:54:23 +0100

    modified script fo functionality

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,101 +1,115 @@
-import evaluate from './service.js'
+const numberBtn = document.querySelectorAll('.number_btn')
+const AC = document.getElementById('AC')
+const plusOrMinus = document.getElementById('plus_or_minus')
+// const percentage = document.getElementById('percentage')
+const operationBtn = document.querySelectorAll('.operations')
+const equals = document.getElementById('equals')
+const currentOperand = document.querySelector('#current_operand span')
+const previousOperand = document.querySelector('#previous_operand span')
 
-const numberButtons = document.querySelectorAll('[data-number')
-const operatorButtons = document.querySelectorAll('[data-operator]')
+let currentOperation = ''
 
-const equalsBtn = document.querySelector('[data-equals]')
-const backBtn = document.querySelector('[data-back]')
-const acButton = document.querySelector('[data-ac]')
-const showAns = document.querySelector('[data-show-ans]')
-const showWorking = document.querySelector('[data-show-working]')
+allClear()
 
-export const resHolder = {
-  answer: null,
-  firstValue: '',
-  secondValue: '',
-  currOperator: ''
+// Clear the calculator
+function allClear () {
+  currentOperand.textContent = ''
+  previousOperand.textContent = ''
+  currentOperation = ''
 }
 
-// const operators = ['+', '-', '*', '÷', '%'] // this to keep track of operators
-
-const blinkShowWorking = () => {
-  showWorking.style.backgroundColor = 'gold'
-  setTimeout(() => {
-    showWorking.style.backgroundColor = '#000'
-  }, 50)
-}
-
-equalsBtn.addEventListener('click', () => {
-  resHolder.firstValue = resHolder.answer
-  resHolder.secondValue = ''
-  showAns.innerHTML = resHolder.answer
-  showWorking.innerHTML = resHolder.answer
-  blinkShowWorking()
-})
-
-acButton.addEventListener('click', () => {
-  showWorking.innerHTML = 0
-  showAns.innerHTML = 0
-  document.getElementById('.').disabled = false
-  resHolder.answer = 0
-  resHolder.currOperator = null
-})
-
-backBtn.addEventListener('click', () => {
-  const text = showWorking.innerHTML
-
-  if (isNaN(text.slice(-1))) document.getElementById('.').disabled = false
-
-  if (!text.includes('.')) document.getElementById('.').disabled = false
-
-  if (isNaN(text.slice(-2).slice(0, 1))) document.getElementById('.').disabled = true
-
-  showWorking.innerHTML = text.slice(0, -1) || 0
-
-  if (resHolder.currOperator) {
-    const vals = showWorking.textContent.split(resHolder.currOperator)
-    evaluate(+vals[0], +vals[1] || 0, resHolder.currOperator)
+// Append number to screen when number button pressed
+function appendNumber (number) {
+  if (number === '.' && currentOperand.textContent.includes('.')) {
+    return
   }
-})
+  number = (currentOperand.textContent += number).toString
+  if (
+    currentOperand.textContent[0] === '0' &&
+     currentOperand.textContent[1] === '0') {
+    currentOperand.textContent = '0'
+  }
+}
 
-numberButtons.forEach((button) => {
+// Update operation
+function updateOperation (operation) {
+  currentOperation = operation
+  if (currentOperand.textContent !== '' && previousOperand.textContent !== '') {
+    return
+  }
+  if (currentOperand.textContent !== '') {
+    previousOperand.textContent = currentOperand.textContent
+    currentOperand.textContent = ''
+  }
+}
+
+// Perform calculation and update display
+function calculate () {
+  let result = ''
+  const b = Number(currentOperand.textContent)
+  const a = Number(previousOperand.textContent)
+  console.log(a, b, currentOperation)
+
+  switch (currentOperation) {
+    case 'plus':
+      result = a + b
+      break
+    case 'minus':
+      result = a - b
+      break
+    case 'times':
+      result = a * b
+      break
+    case 'division':
+      result = a / b
+      break
+    case 'percentage':
+      result = a * (b / 100)
+      break
+  }
+  currentOperand.textContent = result
+  previousOperand.textContent = ''
+  currentOperation = 'equals'
+}
+
+// Change the sign of currentOperand
+function changeSign () {
+  const b = Number(currentOperand.textContent)
+  if (b >= 0) {
+    currentOperand.textContent = '-' + currentOperand.textContent
+  } else {
+    currentOperand.textContent = currentOperand.textContent.substring(1)
+  }
+}
+
+// EVENT LISTENERS
+AC.addEventListener('click', allClear)
+
+// number buttons
+numberBtn.forEach((button) => {
   button.addEventListener('click', () => {
-    if (+showWorking.innerHTML === 0 && showWorking.innerHTML.length <= 1) {
-      if (button.id === '.') {
-        showWorking.innerHTML = '0' + button.id
-        button.disabled = true
-        return
-      }
-
-      showWorking.innerHTML = +button.id
-      return
-    }
-
-    showWorking.textContent += button.id
-    if (button.id === '.') button.disabled = true
-
-    if (resHolder.currOperator) {
-      console.log('operator found', resHolder.currOperator)
-      const vals = showWorking.textContent.split(resHolder.currOperator)
-
-      evaluate(+vals[0], +vals[1] || 0, resHolder.currOperator)
+    if (currentOperation === 'equals') {
+      allClear()
+      appendNumber(button.textContent)
+    } else {
+      appendNumber(button.textContent)
     }
   })
 })
 
-operatorButtons.forEach((button) => {
+// operation buttons
+operationBtn.forEach((button) => {
   button.addEventListener('click', () => {
-    if (showWorking.innerHTML.length <= 0) return
-
-    if (resHolder.answer && resHolder.currOperator) {
-      showWorking.textContent = resHolder.answer
-    }
-
-    showWorking.textContent += button.id
-
-    resHolder.currOperator = button.id
-
-    blinkShowWorking()
-    document.getElementById('.').disabled = false
+    updateOperation(button.id)
   })
+})
+
+// equals button
+equals.addEventListener('click', () => {
+  calculate()
+})
+
+// plusorMinus button
+plusOrMinus.addEventListener('click', () => {
+  changeSign()
 })
